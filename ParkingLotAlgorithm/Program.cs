67d8a00d510648bddd56a7f4cb02f63df5c13f76
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-05-27 21:00:42 -0400

    Add project files.

---
+++ ParkingLotAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkingLotAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
