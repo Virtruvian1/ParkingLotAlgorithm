9cdd1c20b27ac24e3cf378fb9233179ac0ce8c24
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-05-27 23:24:42 -0400

    Create Parking Lot Class

--- ParkingLotAlgorithm/Program.cs
+++ ParkingLotAlgorithm/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Threading;
 
 namespace ParkingLotAlgorithm
 {
+    public static class ParkingLot
+    {
+        public static int ParkingLotSize = 0;
+
+        public static void GetParkingLotSize()
+        {
+            bool validInput = false;
+            int userInput;
+
+            do
+            {
+                Console.Write("Specify the size of the parking lot:\r\n" +
+                              "Range of 0 to 500\r\n" +
+                              "Input: ");
+                Int32.TryParse(Console.ReadLine(), out userInput);
+                // TODO: Add exception handle to catch characters and strings
+                if (userInput >= 0 && userInput <= 500)
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("The size must be 0 to 500.\r\n" +
+                                      "Refreshing in 3 seconds.");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
+            while (!validInput);
+            ParkingLotSize = userInput;
+        }
+
+    }
+    public static class Vehicles 
+    { 
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ParkingLot.GetParkingLotSize();
         }
     }
 }
